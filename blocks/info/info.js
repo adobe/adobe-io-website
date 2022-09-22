d91e7f6fe2195761e0a2e0e82f91ef95ee99d3d1
Meryll Blanchet <mblanche@adobe.com>
2022-09-22 18:41:57 +0200

    feat: info block

---
+++ blocks/info/info.js
@@ -0,0 +1,22 @@
+/**
+ * decorates the info
+ * @param {Element} block The info block element
+ */
+export default function decorate(block) {
+  block.classList.add('spectrum--light');
+  block.querySelectorAll('h2').forEach((h2) => {
+    h2.classList.add('spectrum-Heading', 'spectrum-Heading--sizeM');
+    const hr = document.createElement('hr');
+    hr.classList.add('spectrum-Divider', 'spectrum-Divider--sizeL');
+    h2.after(hr);
+  });
+  block.querySelectorAll('p').forEach((p) => {
+    p.classList.add('spectrum-Body', 'spectrum-Body--sizeM');
+  });
+  block.querySelectorAll('ul').forEach((ul) => {
+    ul.classList.add('spectrum-Body', 'spectrum-Body--sizeM');
+  });
+  block.querySelectorAll('a').forEach((a) => {
+    a.classList.add('spectrum-Link', 'spectrum-Link--quiet');
+  });
+}
