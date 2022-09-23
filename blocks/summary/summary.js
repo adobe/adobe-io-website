4e2e2b4f9038b5fd5c4fc82f462f914a4a3e59e1
Meryll Blanchet <mblanche@adobe.com>
2022-09-23 15:40:38 +0200

    feat: summary block

---
+++ blocks/summary/summary.js
@@ -0,0 +1,34 @@
+/**
+ * decorates the summary
+ * @param {Element} block The summary block element
+ */
+export default function decorate(block) {
+  block.classList.add('spectrum--dark');
+  block.querySelectorAll('h2').forEach((h2) => {
+    h2.classList.add('spectrum-Heading', 'spectrum-Heading--sizeL');
+  });
+  block.querySelectorAll('p').forEach((p) => {
+    const hasLinks = p.querySelectorAll('a, button');
+    // don't attach to icon container or if p tag contains links
+    if (!p.classList.contains('icon-container') && hasLinks.length === 0) {
+      p.classList.add('spectrum-Body', 'spectrum-Body--sizeL');
+    } else if (hasLinks.length > 0) {
+      p.classList.remove('button-container');
+    }
+    hasLinks.forEach((button) => {
+      button.className = '';
+      button.classList.add('spectrum-Button', 'spectrum-Button--secondary', 'spectrum-Button--sizeM', 'spectrum-Button--overBackground');
+    });
+  });
+  // delete image and re-insert as bg
+  const summaryImageSrc = block.querySelector('img') ? block.querySelector('img').src : null;
+  const summaryImageAlt = block.querySelector('img') ? block.querySelector('img').alt : '';
+  const span = document.createElement('span');
+  span.role = 'img';
+  span.setAttribute('aria-label', summaryImageAlt);
+  block.parentElement.parentElement.prepend(span);
+  block.querySelectorAll('picture').forEach((picture) => {
+    picture.parentElement.parentElement.remove();
+  });
+  block.parentElement.parentElement.style.backgroundImage = `url(${summaryImageSrc})`;
+}
