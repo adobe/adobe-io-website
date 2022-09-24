b20295cf2a49bcf4b49a9bffa24ccbecc6a95a61
Meryll Blanchet <mblanche@adobe.com>
2022-09-24 16:43:10 +0200

    fix: rewrote hero block

--- blocks/hero/hero.js
+++ blocks/hero/hero.js
@@ -1,3 +1,8 @@
+import {
+  decorateButtons,
+  removeEmptyPTags,
+} from '../../scripts/lib-adobeio.js';
+
 /**
  * Rearranges the links into a hero-button-container div
  * @param {*} block The hero block element
@@ -13,52 +18,28 @@ function rearrangeLinks(block) {
 }
 
 /**
- * Decorates the links as Spectrum Buttons, removes extra HTML
- * @param {*} block The hero block element
- */
-function decorateLinks(block) {
-  rearrangeLinks(block);
-  // Link decoration as Spectrum Buttons
-  block.querySelectorAll('a').forEach((a) => {
-    a.className = '';
-    a.classList.add('spectrum-Button');
-    a.target = '_blank';
-    a.rel = 'noopener noreferrer';
-    if (a.parentElement.tagName === 'STRONG') {
-      // Primary
-      a.classList.add('spectrum-Button--fill', 'spectrum-Button--accent', 'spectrum-Button--sizeM');
-      const strong = a.parentNode;
-      strong.parentNode.replaceChild(a, strong);
-    } else {
-      // Secondary
-      a.classList.add('spectrum-Button--fill', 'spectrum-Button--secondary', 'spectrum-Button--sizeM');
-    }
-    // Inserts extra span to wrap the link text
-    const span = document.createElement('span');
-    span.textContent = a.textContent;
-    span.classList.add('spectrum-Button-label');
-    a.innerText = '';
-    a.appendChild(span);
-  });
-}
-
-/**
  * decorates the hero
  * @param {Element} block The hero block element
  */
 export default async function decorate(block) {
+  removeEmptyPTags(block);
   // Block decoration
   block.classList.add('spectrum--lightest');
   // H1 decoration
-  block.querySelectorAll('h1').forEach((h1) => {
-    h1.classList.add('spectrum-Heading', 'spectrum-Heading--sizeXXL', 'spectrum-Heading--serif');
+  block.querySelectorAll('h1, h2, h3, h4, h5, h6').forEach((h) => {
+    h.classList.add('spectrum-Heading', 'spectrum-Heading--sizeXXL', 'spectrum-Heading--serif');
+  });
+  block.querySelectorAll('picture source').forEach((picture) => {
+    // Removes weird max-width attribute
+    picture.media = '';
   });
   // Removes content for span.icon
   block.querySelectorAll('span.icon').forEach((span) => {
     span.textContent = '';
   });
   // Link decoration
-  decorateLinks(block);
+  rearrangeLinks(block);
+  decorateButtons(block);
   // Paragraph decoration
   block.querySelectorAll('p').forEach((p) => {
     if (p.innerText) {
