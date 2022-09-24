d460f5949273f278921ca345de00dd636c72722f
Meryll Blanchet <mblanche@adobe.com>
2022-09-24 16:40:52 +0200

    fix: rewrote announcement block

--- blocks/announcement/announcement.js
+++ blocks/announcement/announcement.js
@@ -1,27 +1,16 @@
+import { decorateButtons, removeEmptyPTags } from '../../scripts/lib-adobeio.js';
+
 /**
  * decorates the announcement
  * @param {Element} block The announcement block element
  */
 export default async function decorate(block) {
-  // Decorate h3
-  block.querySelectorAll('h3').forEach((h3) => {
-    h3.classList.add('spectrum-Heading', 'spectrum-Heading--sizeM', 'announce-heading');
+  decorateButtons(block);
+  removeEmptyPTags(block);
+  block.querySelectorAll('h1, h2, h3, h4, h5, h6').forEach((h) => {
+    h.classList.add('spectrum-Heading', 'spectrum-Heading--sizeM', 'announce-heading');
   });
-  // Transforms and decorates links into buttons
-  block.querySelectorAll('p.button-container').forEach((p) => {
-    p.classList.replace('button-container', 'announce-link');
-    p.querySelectorAll('strong > a').forEach((a) => {
-      a.target = '_blank';
-      a.rel = 'noopener noreferrer';
-      a.className = '';
-      a.classList.add('spectrum-Button', 'spectrum-Button--fill', 'spectrum-Button--accent', 'spectrum-Button--sizeM');
-      const span = document.createElement('span');
-      span.className = 'spectrum-Button-label';
-      span.innerText = a.innerText;
-      a.innerText = '';
-      a.append(span);
-      const strong = a.parentNode;
-      p.replaceChild(a, strong);
-    });
+  block.querySelectorAll('p a').forEach((link) => {
+    link.parentElement.classList.add('announce-link');
   });
 }
