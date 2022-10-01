35b86055d3e2e3ff3bd4f3bc128c2b816f023ed7
Meryll Blanchet <mblanche@adobe.com>
2022-10-01 12:03:09 +0200

    chore: simplified embed

--- blocks/embed/embed.js
+++ blocks/embed/embed.js
@@ -4,24 +4,6 @@
  * https://www.hlx.live/developer/block-collection/embed
  */
 
-const loadScript = (url, callback, type) => {
-  const head = document.querySelector('head');
-  const script = document.createElement('script');
-  script.src = url;
-  if (type) {
-    script.setAttribute('type', type);
-  }
-  script.onload = callback;
-  head.append(script);
-  return script;
-};
-
-const getDefaultEmbed = (url) => `<div style="left: 0; width: 100%; height: 0; position: relative; padding-bottom: 56.25%;">
-  <iframe src="${url.href}" style="border: 0; top: 0; left: 0; width: 100%; height: 100%; position: absolute;" allowfullscreen=""
-    scrolling="no" allow="encrypted-media" title="Content from ${url.hostname}" loading="lazy">
-  </iframe>
-</div>`;
-
 const embedYoutube = (url) => {
   const usp = new URLSearchParams(url.search);
   let vid = encodeURIComponent(usp.get('v'));
@@ -35,64 +17,17 @@ const embedYoutube = (url) => {
   return embedHTML;
 };
 
-const embedVimeo = (url) => {
-  const [, video] = url.pathname.split('/');
-  const embedHTML = `<div style="left: 0; width: 100%; height: 0; position: relative; padding-bottom: 56.25%;">
-    <iframe src="https://player.vimeo.com/video/${video}" 
-      style="border: 0; top: 0; left: 0; width: 100%; height: 100%; position: absolute;" 
-      frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen  
-      title="Content from Vimeo" loading="lazy"></iframe>
-    </div>`;
-  return embedHTML;
-};
-
-const embedTwitter = (url) => {
-  const embedHTML = `<blockquote class="twitter-tweet"><a href="${url.href}"></a></blockquote>`;
-  loadScript('https://platform.twitter.com/widgets.js');
-  return embedHTML;
-};
-
-const loadEmbed = (block, link) => {
+const loadEmbed = (block, a) => {
   if (block.classList.contains('embed-is-loaded')) {
     return;
   }
-
-  const EMBEDS_CONFIG = [
-    {
-      match: ['youtube', 'youtu.be'],
-      embed: embedYoutube,
-    },
-    {
-      match: ['vimeo'],
-      embed: embedVimeo,
-    },
-    {
-      match: ['twitter'],
-      embed: embedTwitter,
-    },
-  ];
-
-  const config = EMBEDS_CONFIG.find((e) => e.match.some((match) => link.includes(match)));
+  const link = a.href;
   const url = new URL(link);
-  if (config) {
-    block.innerHTML = config.embed(url);
-    block.classList = `block embed embed-${config.match[0]}`;
-  } else {
-    block.innerHTML = getDefaultEmbed(url);
-    block.classList = 'block embed';
-  }
+  a.insertAdjacentHTML('afterend', embedYoutube(url));
+  block.classList = 'block embed embed-youtube';
+  a.remove();
   block.classList.add('embed-is-loaded');
   block.parentElement.parentElement.classList.add('spectrum--lightest');
-  block.querySelectorAll('h1, h2, h3, h4, h5, h6').forEach((header) => {
-    header.classList.add('spectrum-Heading', 'spectrum-Heading--sizeM');
-  });
-  block.querySelectorAll('p').forEach((p) => {
-    const hasLinks = p.querySelectorAll('a, button');
-    // don't attach to icon container or if p tag contains links
-    if (!p.classList.contains('icon-container') && hasLinks.length === 0) {
-      p.classList.add('spectrum-Body', 'spectrum-Body--sizeM');
-    }
-  });
 };
 
 /**
@@ -100,12 +35,11 @@ const loadEmbed = (block, link) => {
  * @param {*} block The embed block
  */
 export default async function decorate(block) {
-  const link = block.querySelector('a').href;
-  block.textContent = '';
+  const a = block.querySelector('a');//.href;
   const observer = new IntersectionObserver((entries) => {
     if (entries.some((e) => e.isIntersecting)) {
       observer.disconnect();
-      loadEmbed(block, link);
+      loadEmbed(block, a);
     }
   });
   observer.observe(block);
