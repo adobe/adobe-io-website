b61af9d367f49811386ede6c146e25ca409894db
alvaro <aarce@adobe.com>
2023-02-20 18:04:05 +0100

    Included the spectrum css style for the table

--- hlx_statics/blocks/table/table.js
+++ hlx_statics/blocks/table/table.js
@@ -12,15 +12,20 @@ function buildCell(rowIndex) {
 
 export default async function decorate(block) {
   const table = document.createElement('table');
+  table.classList.add('spectrum-Table','spectrum-Table--sizeM','spectrum-Table--spacious');
   const thead = document.createElement('thead');
+  thead.classList.add('spectrum-Table-head');
   const tbody = document.createElement('tbody');
+  tbody.classList.add('spectrum-Table-body');
   table.append(thead, tbody);
   [...block.children].forEach((child, i) => {
     const row = document.createElement('tr');
+    row.classList.add('spectrum-Table-row');
     if (i) tbody.append(row);
     else thead.append(row);
     [...child.children].forEach((col) => {
       const cell = buildCell(i);
+      cell.classList.add('spectrum-Table-cell');
       cell.innerHTML = col.innerHTML;
       row.append(cell);
     });
