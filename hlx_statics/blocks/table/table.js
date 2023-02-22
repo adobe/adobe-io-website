96461c0a82cb99b535eb9e27bb2f948a2233e1ba
alvaro <aarce@adobe.com>
2023-02-22 14:02:03 +0100

    Fixed issues based on revisions

--- hlx_statics/blocks/table/table.js
+++ hlx_statics/blocks/table/table.js
@@ -10,7 +10,14 @@ function buildCell(rowIndex) {
   return cell;
 }
 
+function buildCellHead(rowIndex) {
+  const cell = document.createElement('th');
+  if (!rowIndex) cell.setAttribute('scope', 'col');
+  return cell;
+}
+
 export default async function decorate(block) {
+  block.setAttribute('dir','ltr');
   const table = document.createElement('table');
   table.classList.add('spectrum-Table','spectrum-Table--sizeM','spectrum-Table--spacious');
   const thead = document.createElement('thead');
@@ -20,15 +27,24 @@ export default async function decorate(block) {
   table.append(thead, tbody);
   [...block.children].forEach((child, i) => {
     const row = document.createElement('tr');
-    row.classList.add('spectrum-Table-row');
-    if (i) tbody.append(row);
-    else thead.append(row);
-    [...child.children].forEach((col) => {
-      const cell = buildCell(i);
-      cell.classList.add('spectrum-Table-cell');
-      cell.innerHTML = col.innerHTML;
-      row.append(cell);
-    });
+    if (i) { 
+      row.classList.add('spectrum-Table-row');
+      tbody.append(row);
+      [...child.children].forEach((col) => {
+        const cell = buildCell(i);
+        cell.classList.add('spectrum-Table-cell');
+        cell.innerHTML = col.innerHTML;
+        row.append(cell);
+      });}
+    else { 
+      thead.append(row);
+      [...child.children].forEach((col) => {
+        const cell = buildCellHead(i);
+        cell.classList.add('spectrum-Table-headCell');
+        cell.innerHTML = col.innerHTML;
+        row.append(cell);
+      });}
+
   });
   block.innerHTML = '';
   block.append(table);
