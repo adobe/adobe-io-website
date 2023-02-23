e1410d10ce49fec02d25625b442de523e8ed6c36
timkim <timk@adobe.com>
2023-02-22 16:19:41 -0800

    Add in iframe component

---
+++ hlx_statics/blocks/iframe/iframe.js
@@ -0,0 +1,17 @@
+import {
+    createTag
+  } from '../../scripts/lib-adobeio.js';
+
+/**
+ * decorates the iframe
+ * @param {Element} block The hero block element
+ */
+export default async function decorate(block) {
+    const iframeSrc = block.querySelector('a');
+
+    const iframeContainer = block.parentElement;
+    const iframe = createTag('iframe', { class: 'iframe-container', 'src': iframeSrc.href });
+    iframeContainer.append(iframe);
+    block.remove();
+
+}
