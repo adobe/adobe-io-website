fdf86cd18445f6e844cc878209dbb818d329a761
louisachu <louisa@ensemble.com>
2023-03-03 16:09:01 -0800

    adding mobile css to show the header properly

--- hlx_statics/blocks/header/header.js
+++ hlx_statics/blocks/header/header.js
@@ -52,15 +52,16 @@ function globalSignIn() {
 function globalNavLinkItemDropdown(id, name, links) {
   return `
       <button id="nav-dropdown-button_${id}" class="spectrum-Picker spectrum-Picker--sizeM spectrum-Picker--quiet navigation-dropdown" aria-haspopup="listbox">
-        <span class="spectrum-Picker-label">
+        <span class="spectrum-Picker-label nav-label">
           ${name}
         </span>
-        <svg class="spectrum-Icon spectrum-UIIcon-ChevronDown100 spectrum-Picker-menuIcon" focusable="false" aria-hidden="true">
-          <use xlink:href="#spectrum-css-icon-Chevron100" />
-        </svg>
+        <svg aria-hidden="true" role="img" focusable="false" class="spectrum-Icon spectrum-UIIcon-ChevronDown100 spectrum-Picker-menuIcon dropdown-icon">
+          <path d="M4.5 13.25a1.094 1.094 0 01-.773-1.868L8.109 7 3.727 2.618A1.094 1.094 0 015.273 1.07l5.157 5.156a1.094 1.094 0 010 1.546L5.273 12.93a1.091 1.091 0 01-.773.321z" class="spectrum-UIIcon--large"></path>
+          <path d="M3 9.95a.875.875 0 01-.615-1.498L5.88 5 2.385 1.547A.875.875 0 013.615.302L7.74 4.377a.876.876 0 010 1.246L3.615 9.698A.872.872 0 013 9.95z" class="spectrum-UIIcon--medium"></path>
+         </svg>
       </button>
-      <div id="nav-dropdown-popover_${id}" class="spectrum-Popover spectrum-Popover--bottom spectrum-Picker-popover spectrum-Picker-popover--quiet filter-by-popover nav-dropdown-popover">
-        <ul class="spectrum-Menu" role="menu">
+      <div id="nav-dropdown-popover_${id}" class="nav-dropdown-popover">
+        <ul class="nav-sub-menu spectrum-Menu" role="menu">
           ${links}
         </ul>
       </div>
@@ -101,7 +102,7 @@ const searchFrameOnLoad = (renderedFrame, counter = 0, loaded) => {
   // and received a confirmation from the iframe
   if (!loaded) {
     const queryString = getQueryString();
-    
+
     if (queryString.has('query')) {
       const searchIframeContainer = document.querySelector('div.nav-console-search-frame');
       if (searchIframeContainer.length > 0) {
@@ -212,6 +213,11 @@ export default async function decorate(block) {
     header.classList.add('main-header', 'global-nav-header');
     header.setAttribute('daa-lh', 'header');
 
+    const mobileButton = createTag('input', {class: 'menu-btn', type: 'checkbox', id:'menu-btn'});
+    header.appendChild(mobileButton);
+    const mobileMenu = createTag('label', {class: 'menu-icon', for: "menu-btn"} );
+    mobileMenu.innerHTML = '<span class="navicon"></span>';
+    header.appendChild(mobileMenu);
     const iconContainer = createTag('p', { class: 'icon-adobe-container' });
     const title = block.querySelector('p:nth-child(1)');
     const siteLink = title.querySelector('strong > a');
@@ -225,6 +231,7 @@ export default async function decorate(block) {
 
     const ul = block.querySelector('ul');
     ul.setAttribute('id', 'navigation-links');
+    ul.setAttribute('class', 'menu');
     ul.style.listStyleType = 'none';
 
     if (isTopLevelNav(window.location.pathname)) {
