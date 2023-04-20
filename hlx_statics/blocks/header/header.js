ceb73c77ddafbcad9fcee43df96576fd2a3d0319
louisachu <louisa@ensemble.com>
2023-04-20 13:34:34 -0700

    Update css of the distribute button

--- hlx_statics/blocks/header/header.js
+++ hlx_statics/blocks/header/header.js
@@ -23,7 +23,7 @@ function globalNavSearchButton() {
 
 function globalDistributeButton() {
   const div = createTag('div', { class: 'nav-console-distribute-button' });
-  div.innerHTML = `<a href="/distribute" class="spectrum-Button spectrum-Button--secondary  spectrum-Button--sizeM">
+  div.innerHTML = `<a href="/distribute" class="spectrum-Button spectrum-Button--cta spectrum-Button-fill  spectrum-Button--sizeM">
     <span class="spectrum-Button-label">
       Distribute
     </span>
