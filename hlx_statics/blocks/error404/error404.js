35d718f6be3d1e05472240734310471b39d1221a
timkim <timk@adobe.com>
2023-06-15 13:26:40 -0700

    rename to error404

--- hlx_statics/blocks/error404/error404.js
+++ hlx_statics/blocks/error404/error404.js
@@ -1,7 +1,7 @@
 import { decorateButtons } from '../../scripts/lib-adobeio.js';
 
 /**
- * decorates the 404
+ * decorates the error404
  * @param {Element} block The 404 block element
  */
 export default async function decorate(block) {
