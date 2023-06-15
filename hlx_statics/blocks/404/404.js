f2060f8e4944aae2d85dd5750dba66892d624b00
timkim <timk@adobe.com>
2023-06-15 13:22:40 -0700

    Add in 404 style

---
+++ hlx_statics/blocks/404/404.js
@@ -0,0 +1,10 @@
+import { decorateButtons } from '../../scripts/lib-adobeio.js';
+
+/**
+ * decorates the 404
+ * @param {Element} block The 404 block element
+ */
+export default async function decorate(block) {
+  decorateButtons(block);
+}
+
