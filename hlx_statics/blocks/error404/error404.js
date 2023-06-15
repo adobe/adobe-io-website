e30f3e44a1eb2d359090349553c4b09529181724
timkim <timk@adobe.com>
2023-06-15 13:38:50 -0700

    add in 404 block

--- hlx_statics/blocks/error404/error404.js
+++ hlx_statics/blocks/error404/error404.js
@@ -6,5 +6,9 @@ import { decorateButtons } from '../../scripts/lib-adobeio.js';
  */
 export default async function decorate(block) {
   decorateButtons(block);
+
+  block.querySelectorAll('h1').forEach((h1) => {
+    h1.classList.add('spectrum-Heading--sizeXXL', 'spectrum-Heading--serif');
+  });
 }
 
