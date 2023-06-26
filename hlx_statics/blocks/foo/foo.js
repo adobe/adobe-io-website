78087ef0935333a294e3010fc42ca97cc99aee54
Diane Batres <dbatres@Dianes-Laptop.local>
2023-06-26 11:17:49 -0700

    foo no added attributes

--- hlx_statics/blocks/foo/foo.js
+++ hlx_statics/blocks/foo/foo.js
@@ -33,22 +33,23 @@ export default async function decorate(block) {
   decorateButtons(block);
   removeEmptyPTags(block);
 
-  block.setAttribute('daa-lh', 'foo');
+//   block.setAttribute('daa-lh', 'foo');
   
-  block.querySelectorAll('h1, h2, h3, h4, h5, h6').forEach((h) => {
-    h.classList.add('spectrum-Heading', 'spectrum-Heading--sizeL', 'foo-heading');
-  });
-  block.querySelectorAll('p').forEach((p) => {
-    p.classList.add('spectrum-Body', 'spectrum-Body--sizeL');
-  });
-  block.querySelectorAll('p').forEach((paragraph) => {
-    paragraph.classList.add('spectrum-Body');
-    paragraph.classList.add('spectrum-Body--sizeL');
-  });
-  block.querySelectorAll('p a').forEach((link) => {
-    link.parentElement.classList.add('foo-link');
-  });
-  block
+//   block.querySelectorAll('h1, h2, h3, h4, h5, h6').forEach((h) => {
+//     h.classList.add('spectrum-Heading', 'spectrum-Heading--sizeL', 'foo-heading');
+//   });
+//   block.querySelectorAll('p').forEach((p) => {
+//     p.classList.add('spectrum-Body', 'spectrum-Body--sizeL');
+//   });
+//   block.querySelectorAll('p').forEach((paragraph) => {
+//     paragraph.classList.add('spectrum-Body');
+//     paragraph.classList.add('spectrum-Body--sizeL');
+//   });
+//   block.querySelectorAll('p a').forEach((link) => {
+//     link.parentElement.classList.add('foo-link');
+//   });
+//   block
+
   calculateOverlapping(block);
 }
 
