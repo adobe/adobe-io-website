f6fdc950521fef06f886e1f3ab0aba7da10f367a
Diane Batres <dbatres@Dianes-Laptop.local>
2023-07-13 14:53:48 -0700

    container spacing of image and text

--- hlx_statics/blocks/carousel/carousel.js
+++ hlx_statics/blocks/carousel/carousel.js
@@ -1,5 +1,4 @@
-import { createTag, decorateButtons } from '../../scripts/lib-adobeio.js';
-
+import { createTag, decorateButtons, removeEmptyPTags} from '../../scripts/lib-adobeio.js';
 
 /**
  * decorates the carousel
@@ -7,43 +6,43 @@ import { createTag, decorateButtons } from '../../scripts/lib-adobeio.js';
  */
 export default async function decorate(block) {
   decorateButtons(block);
+  removeEmptyPTags(block);
 
   block.querySelectorAll('h1, h2, h3, h4, h5, h6').forEach((h) => {
-    //add everything but image to a div
-    const flex_div = createTag('div', { id: 'flex_div' });
+    //create flex display
+    h.parentElement.classList.add('carousel-container');
+    h.classList.add('spectrum-Heading', 'spectrum-Heading--sizeL');
+
+    h.parentElement.setAttribute('id', h.id);
+
+    //add everything but image to the left div
+    let flex_div = createTag('div', { id: 'left-flex-div-'+h.id});
     h.parentElement.append(flex_div);
     flex_div.append(h);
 
   });
 
-  const flex_div = document.getElementById('flex_div');
-
-  block.querySelectorAll('p').forEach(function (p){
-    p.childNodes.forEach(function (child) {
-        // console.log(child.nodeName);
-        if(child.nodeName === 'PICTURE'){
-            
-            p.classList.add('spectrum-Body', 'spectrum-Body--sizeL');
-        } else {
-            console.log(child.nodeName);
-            flex_div.appendChild(p);
-        };
-    });
-    
-  });
-
-  block.querySelectorAll('h1, h2, h3, h4, h5, h6').forEach((h) => {
-    h.classList.add('spectrum-Heading', 'spectrum-Heading--sizeL');
-    //make a flex box - row
-    h.parentElement.parentElement.classList.add('carousel-format');
-  });
-
   block.querySelectorAll('img').forEach((img) => {
     img.classList.add('img-size');
+    img.parentElement.parentElement.setAttribute('id', 'IMAGE');
+
+    //add image to right div
+    let flex_div = createTag('div', { id: 'right-flex-div-'+img.parentElement.parentElement.parentElement.id});
+    img.parentElement.parentElement.parentElement.append(flex_div);
+    flex_div.append(img.parentElement.parentElement);
+    flex_div.classList.add('right-container');
   });
-  
 
-
-  
+  block.querySelectorAll('p').forEach(function (p){
+    //add everything but image to the left div
+    if(p.id === "IMAGE"){
+        p.classList.add('spectrum-Body', 'spectrum-Body--sizeS');
+    }else{
+        let flex_div = document.getElementById('left-flex-div-' + p.parentElement.id); 
+        flex_div.setAttribute('class', 'left-container');
+        p.classList.add('spectrum-Body', 'spectrum-Body--sizeL');
+        flex_div.append(p);
+    }
+  });
 }
 
