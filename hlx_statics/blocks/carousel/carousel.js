cbd206875e65c1421763ca897ae4d253fa04fd7f
Diane Batres <dbatres@dianes-laptop.corp.adobe.com>
2023-07-14 13:43:29 -0700

    vanilla js slider

--- hlx_statics/blocks/carousel/carousel.js
+++ hlx_statics/blocks/carousel/carousel.js
@@ -8,12 +8,41 @@ export default async function decorate(block) {
   removeEmptyPTags(block);
   decorateButtons(block);
 
+  //create section tag for animation
+  const carousel_block = document.getElementsByClassName('carousel block')[0];
+  const carousel_section = createTag('section', {class: 'slider-wrapper'});
+  carousel_block.append(carousel_section);
+
+  //add slide arrow buttons
+  const arrow_button_previous = createTag('button', {class: 'slide-arrow'});
+  arrow_button_previous.setAttribute('id', 'slide-arrow-previous');
+  arrow_button_previous.innerHTML = '&#8249;'
+  const arrow_button_forward = createTag('button', {class: 'slide-arrow'});
+  arrow_button_forward.setAttribute('id', 'slide-arrow-forward');
+  arrow_button_forward.innerHTML = '&#8250;'
+  carousel_section.append(arrow_button_previous);
+  carousel_section.append(arrow_button_forward);
+
+  //add ul tag to contain carousel slides
+  const carousel_ul = createTag('ul', {class: 'slides-container'});
+  carousel_ul.setAttribute('id', 'slides-container');
+  carousel_section.append(carousel_ul);
+
   block.querySelectorAll('h1, h2, h3, h4, h5, h6').forEach((h) => {
     //create flex display
     h.parentElement.classList.add('carousel-container');
+    //add section tag for animation
+    h.parentElement.parentElement.replaceWith(carousel_section);
     h.classList.add('spectrum-Heading', 'spectrum-Heading--sizeL', 'carousel-heading');
 
+    //name div for convenience and add divs to section tag
     h.parentElement.setAttribute('id', h.id);
+
+    //create li tag to hold carousel slide (div)
+    let carousel_li = createTag('li', {class: 'slide'});
+    carousel_li.setAttribute('id', h.id);
+    carousel_ul.append(carousel_li);
+    carousel_li.append(h.parentElement);
 
     //add everything but image to the left div
     let flex_div = createTag('div', { id: 'left-flex-div-'+h.id});
@@ -54,5 +83,23 @@ export default async function decorate(block) {
         }
     };
   });
+
+  //slide functionality with arrow buttons
+  const slidesContainer = document.getElementById("slides-container");
+  const slide = document.querySelector(".slide");
+  const prevButton = document.getElementById("slide-arrow-previous");
+  const nextButton = document.getElementById("slide-arrow-forward");
+
+  nextButton.addEventListener("click", () => {
+    const slideWidth = slide.clientWidth;
+      slidesContainer.scrollLeft += slideWidth;
+  });
+
+  prevButton.addEventListener("click", () => {
+    const slideWidth = slide.clientWidth;
+    slidesContainer.scrollLeft -= slideWidth;
+  });
+  
+
 }
 
