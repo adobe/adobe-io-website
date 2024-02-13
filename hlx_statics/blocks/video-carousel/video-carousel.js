726e3dd1dbe58dc163576c5f8a331c317957ae46
louisachu <louisa@ensemble.com>
2024-02-12 16:24:51 -0800

    update

--- hlx_statics/blocks/video-carousel/video-carousel.js
+++ hlx_statics/blocks/video-carousel/video-carousel.js
@@ -188,7 +188,7 @@ export default async function decorate(block) {
     } else {
       // Render the url link through video tag within right container of one of the video carousel slide.
       html = `<div style="left: 0; width: 100%; height: 0; position: relative; padding-bottom: 56.25%;">
-        <video loading="lazy" style="border: 0; top: 0; left: 0; width: 100%; height: 100%; position: absolute;" preload="metadata" playsinline muted>
+        <video loading="lazy" style="border: 0; top: 0; left: 0; width: 100%; height: 100%; position: absolute;" autoplay="true" preload="metadata" playsinline muted>
           <source src="${url}" />
         </video>
       </div>`;
