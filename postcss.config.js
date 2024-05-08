4ff5e8814b984a33467e159aa0c9230b83e34415
timkim <timk@adobe.com>
2024-05-08 15:51:29 -0700

    Retry with spectrum-build branch

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,8 @@
 module.exports = {
   plugins: [
-    require('postcss-import'),
-    require('postcss-varfallback'),
-    require('postcss-dropunusedvars'),
+    require("postcss-import"),
+    require("postcss-varfallback"),
+    require("postcss-dropunusedvars"),
+    require("cssnano"),
   ],
 };
