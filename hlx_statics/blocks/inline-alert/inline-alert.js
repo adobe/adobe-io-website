dd9ed074f1f063b3d1cef221a64a82f9f19dc43d
timkim <timk@adobe.com>
2024-05-10 15:21:19 -0700

    Replace inline alert content into what spectrum wants

--- hlx_statics/blocks/inline-alert/inline-alert.js
+++ hlx_statics/blocks/inline-alert/inline-alert.js
@@ -1,6 +1,6 @@
 import {
     createTag,
-    decorateAnchorLink
+    getBlockSectionContainer
   } from '../../scripts/lib-adobeio.js';
 
   /**
@@ -9,6 +9,25 @@ import {
  */
 export default async function decorate(block) {
     const container = getBlockSectionContainer(block);
-    block.classList.add('spectrum-InLineAlert');
+    block.querySelectorAll('.inline-alert > div > div').forEach((inlineAlert) => {
+        inlineAlert.classList.add('spectrum-InLineAlert');
+        // replace header with spectrum div
+        inlineAlert.querySelectorAll('h1').forEach((header) =>{
+            const divHeader = createTag('div', { class: 'spectrum-InLineAlert-header' });
+            const parent = header.parentElement;
+            divHeader.textContent = header.textContent;
+            parent.appendChild(divHeader);
+            header.replaceWith(divHeader);
+        });
+
+        inlineAlert.querySelectorAll('p').forEach((p) =>{
+            const divP = createTag('div', { class: 'spectrum-InLineAlert-content' });
+            const parent = p.parentElement;
+            divP.textContent = p.textContent;
+            parent.appendChild(divP);
+            p.replaceWith(divP);
+        });
+    })
+
 }
   
