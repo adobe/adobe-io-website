59f26cfb955f4e7642763bd4326396d086a97aa5
prathap <prathap@gmail.com>
2024-07-10 20:13:39 +0530

    Added Profile-card

---
+++ hlx_statics/blocks/profile-card/profile-card.js
@@ -0,0 +1,34 @@
+import { applyBkgColorOverride,applyWidthOverride,decorateButtons} from '../../scripts/lib-adobeio.js';
+
+/**
+ * decorates the title
+ * @param {Element} block The title block element {Parameter Type} Name of the Parameter
+ */
+export default async function decorate(block) {
+  const fontcolor = block?.parentElement?.parentElement?.getAttribute('data-fontcolor');
+  decorateButtons(block);
+  block.setAttribute('daa-lh', 'profile-card');
+  block.querySelectorAll('h1, h2, h3, h4, h5, h6').forEach((h) => {
+    h.classList.add('spectrum-Heading', 'spectrum-Heading--sizeL', 'title-heading');
+    h.style.color = fontcolor;
+  });
+  block.querySelectorAll('p').forEach((p) => {
+    p.classList.add('spectrum-Body', 'spectrum-Body--sizeL');
+    p.style.color = fontcolor;
+  });
+  applyBkgColorOverride(block);
+  applyWidthOverride(block);
+
+
+  
+  Array.from(block.children).forEach((div)=>{
+    const newDiv=document.createElement('div');
+    newDiv.classList.add('all-button-container')
+  
+    div.lastElementChild.querySelectorAll('.button-container').forEach((p)=>{
+      newDiv.append(p);
+    })
+    
+    div.lastElementChild.append(newDiv)
+  })
+}
