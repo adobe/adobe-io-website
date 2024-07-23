5d234a963d5e96221857bf68c05f1d0fdbb26c33
Melissa Garcia <melissag@ensemble.com>
2024-07-23 10:58:38 -0700

    copy and paste from aem block collection

--- hlx_statics/blocks/table/table.js
+++ hlx_statics/blocks/table/table.js
@@ -1,8 +1,3 @@
-import {
-  applyWidthOverride,
-  applyBkgColorOverride,
-  applyAnalyticHeaderOverride,
-} from '../../scripts/lib-adobeio.js';
 /*
  * Table Block
  * Recreate a table
@@ -10,51 +5,32 @@ import {
  */
 
 function buildCell(rowIndex) {
-  const cell = document.createElement('td');
-  if (!rowIndex) cell.setAttribute('scope', 'col');
-  return cell;
-}
-
-function buildCellHead(rowIndex) {
-  const cell = document.createElement('th');
+  const cell = rowIndex ? document.createElement('td') : document.createElement('th');
   if (!rowIndex) cell.setAttribute('scope', 'col');
   return cell;
 }
 
 export default async function decorate(block) {
-  block.setAttribute('daa-lh', 'table');
-  block.setAttribute('dir','ltr');
   const table = document.createElement('table');
-  table.classList.add('spectrum-Table','spectrum-Table--sizeM','spectrum-Table--spacious');
   const thead = document.createElement('thead');
-  thead.classList.add('spectrum-Table-head');
   const tbody = document.createElement('tbody');
-  tbody.classList.add('spectrum-Table-body');
-  table.append(thead, tbody);
+
+  const header = !block.classList.contains('no-header');
+  if (header) {
+    table.append(thead);
+  }
+  table.append(tbody);
+
   [...block.children].forEach((child, i) => {
     const row = document.createElement('tr');
-    if (i) { 
-      row.classList.add('spectrum-Table-row');
-      tbody.append(row);
-      [...child.children].forEach((col) => {
-        const cell = buildCell(i);
-        cell.classList.add('spectrum-Table-cell');
-        cell.innerHTML = col.innerHTML;
-        row.append(cell);
-      });}
-    else { 
-      thead.append(row);
-      [...child.children].forEach((col) => {
-        const cell = buildCellHead(i);
-        cell.classList.add('spectrum-Table-headCell');
-        cell.innerHTML = col.innerHTML;
-        row.append(cell);
-      });}
-
+    if (header && i === 0) thead.append(row);
+    else tbody.append(row);
+    [...child.children].forEach((col) => {
+      const cell = buildCell(header ? i : i + 1);
+      cell.innerHTML = col.innerHTML;
+      row.append(cell);
+    });
   });
-  block.textContent = '';
+  block.innerHTML = '';
   block.append(table);
-  applyBkgColorOverride(block);
-  applyWidthOverride(block);
-  applyAnalyticHeaderOverride(block);
 }
