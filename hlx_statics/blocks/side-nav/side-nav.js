234c05d994550fc6f78b493a79fd83864609974b
timkim <timk@adobe.com>
2024-08-05 17:04:45 -0700

    Set grid area name

--- hlx_statics/blocks/side-nav/side-nav.js
+++ hlx_statics/blocks/side-nav/side-nav.js
@@ -1,15 +1,10 @@
-import {
-  removeEmptyPTags,
-  decorateButtons,
-  createTag,
-  applyAnalyticHeaderOverride,
-} from '../../scripts/lib-adobeio.js';
-
 /**
  * decorates the side-nav
  * @param {Element} block The site-nav block element
  */
 export default async function decorate(block) {
- console.log('side naving')
- console.log(block)
+  let sideNavContainer = document.querySelector('.side-nav-container');
+  if(sideNavContainer) {
+    sideNavContainer.style.gridArea = 'sidenav';
+  }
 }
