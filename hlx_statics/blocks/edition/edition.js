3c14e1a6298f6b6edbaae8291ab4c62463a3a7fb
Manimaran Kottursamy <mit80447@adobe.com>
2024-08-19 19:45:53 +0530

    Fixed the design issue

--- hlx_statics/blocks/edition/edition.js
+++ hlx_statics/blocks/edition/edition.js
@@ -3,11 +3,15 @@
  * @param {Element} block The title block element {Parameter Type} Name of the Parameter
  */
 export default async function decorate(block) {
-    block.querySelectorAll('.edition > div > div').forEach((div) => {
-        Object.assign(div.style, {
-            backgroundColor: "rgb(187 2 2)",
-            color: "white",
-            fontSize: "12px"
-        });
-    })
+    // const backgroundColor = block?.parentElement?.parentElement?.getAttribute('data-Backgroundcolor');
+    // const fontColor = block?.parentElement?.parentElement?.getAttribute('data-FontColor');
+    // const fontSize = block?.parentElement?.parentElement?.getAttribute('data-FontSize');
+
+    // block.querySelectorAll('.edition > div > div').forEach((div) => {
+    //     Object.assign(div.style, {
+    //         backgroundColor: "rgb(187 2 2)",
+    //         color: "white",
+    //         fontSize: "12px"
+    //     });
+    // })
 }
