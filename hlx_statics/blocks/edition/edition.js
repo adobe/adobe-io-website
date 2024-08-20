0bf8d8b5ef16a139aa1dc2ab7f1a9a77ed4870f1
Manimaran Kottursamy <mit80447@adobe.com>
2024-08-20 17:08:44 +0530

    Edition block elements rearranged

--- hlx_statics/blocks/edition/edition.js
+++ hlx_statics/blocks/edition/edition.js
@@ -9,9 +9,29 @@ export default async function decorate(block) {
 
     block.querySelectorAll('.edition > div > div').forEach((div) => {
         Object.assign(div.style, {
-            backgroundColor: "rgb(187 2 2)",
-            color: "white",
-            fontSize: "12px"
+            backgroundColor: backgroundColor ? backgroundColor : "rgb(187 2 2)",
+            color: fontColor ? fontColor : "white",
+            fontSize: fontSize ? fontSize : "12px"
         });
     })
+
+    const contentElement = document.querySelector('.edition > div > div > p:first-of-type');
+    const spanElement = document.createElement('span');
+    spanElement.innerHTML = contentElement.innerHTML;
+
+    for (let i = 0; i < contentElement.attributes.length; i++) {
+        let attr = contentElement.attributes[i];
+        spanElement.setAttribute(attr.name, attr.value);
+    }
+
+    contentElement.parentNode.replaceChild(spanElement, contentElement);
+    const removeChildNode = document.querySelector('.edition > div > div > p');
+
+    if (removeChildNode) {
+        const anchorTag = removeChildNode.querySelector('a');
+        if (anchorTag) {
+            removeChildNode.parentNode.replaceChild(anchorTag, removeChildNode);
+            spanElement.appendChild(anchorTag);
+        }
+    }
 }
