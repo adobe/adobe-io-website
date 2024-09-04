521de7c64f46d21994fc36192931ca6773f3c4f4
Manimaran Kottursamy <mit80447@adobe.com>
2024-09-04 13:15:45 +0530

    reused the decoratelink component

--- hlx_statics/blocks/link/link.js
+++ hlx_statics/blocks/link/link.js
@@ -1,19 +1,14 @@
+import decorateLink from '../../components/link.js';
 /**
  * decorates the link
  * @param {Element} block The link block element
- */
+*/
 export default async function decorate(block) {
     const variant = block?.parentElement?.parentElement?.getAttribute('data-variant');
-    const anchorTag = block.querySelector('a');
-    block.appendChild(anchorTag);
+    const link = block.querySelector('a');
+    block.appendChild(link);
     const nestedDiv = block.querySelector('div');
     block.removeChild(nestedDiv);
-    if (anchorTag) {
-        anchorTag.classList.remove('button', 'primary');
-        if (variant === 'secondary') {
-            anchorTag.classList.add('spectrum-Link', 'spectrum-Link--quiet', 'spectrum-Link--secondary');
-        } else {
-            anchorTag.classList.add('spectrum-Link', 'spectrum-Link--quiet', 'spectrum-Link--primary');
-        }
-    }
+    link.classList.remove('button', 'primary');
+    decorateLink({link,variant:variant});
 }
