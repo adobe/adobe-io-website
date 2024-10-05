e192d5d8e0bdc74156c2144bd8612db35227ee14
timkim <timk@adobe.com>
2024-10-04 17:36:05 -0700

    Read in side nav and start putting it in html element form

--- hlx_statics/blocks/side-nav/side-nav.js
+++ hlx_statics/blocks/side-nav/side-nav.js
@@ -1,3 +1,6 @@
+import {
+  createTag
+} from '../../scripts/lib-adobeio.js';
 import {getMetadata } from '../../scripts/lib-helix.js';
 /**
  * decorates the side-nav
@@ -12,16 +15,39 @@ export default async function decorate(block) {
     navPath = `${window.location.origin}/${pathPrefix}/config`;
   }
   
+  const navigationLinks = createTag('nav', { role: 'navigation'});
+  navigationLinks.setAttribute('aria-label', 'Primary');
+
+  const navigationLinksContainer = createTag('div');
+  navigationLinks.append(navigationLinksContainer);
+
+  const navigationLinksUl = createTag('ul', {role: 'tree'});
+  navigationLinksUl.setAttribute('aria-label', 'Table of contents');
+  navigationLinksContainer.append(navigationLinksUl);
+
   const resp = await fetch(`${navPath}.plain.html`);
 
   // TODO can be smarter on when to grab the nav 
   if (resp.ok) {
     const html = await resp.text();
-    block.innerHTML = html;
+
+    const parser = new DOMParser();
+    const htmlDocument = parser.parseFromString(html, "text/html");
+
+    [...htmlDocument.querySelectorAll("p")].forEach((item) => {
+      if(item.innerText === 'subPages:') {
+        let sideNavItems = item.parentElement.querySelector('ul');
+        navigationLinksUl.innerHTML += sideNavItems.innerHTML.replaceAll('<p>', '').replaceAll('</p>','');
+      }
+    });
+
+  } else {
+    // TODO: figure out what to do when side nav not present?
   }
 
   let sideNavContainer = document.querySelector('.side-nav-container');
   if(sideNavContainer) {
     sideNavContainer.style.gridArea = 'sidenav';
   }
+  block.append(navigationLinks);
 }
