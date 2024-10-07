e465bb07b062e7055366b112dbc339e4a1a12dc3
BaskarMitrah <mit44186@adobe.com>
2024-10-07 20:50:58 +0530

    Fix : Heading alignment

--- hlx_statics/blocks/heading1/heading1.js
+++ hlx_statics/blocks/heading1/heading1.js
@@ -1,5 +1,9 @@
 import decorateHeading from '../../components/heading.js';
 
 export default function decorate(block) {
+  const heading1Wrapper = document.querySelector('.heading1-wrapper');
+  const nextWrapper = heading1Wrapper.nextElementSibling;
+  const p = nextWrapper.querySelector('p');
+  p.classList.add('header-content');
   decorateHeading({ level: 1, block });
 }
