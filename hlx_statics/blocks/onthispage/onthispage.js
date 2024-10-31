0397ce580f10d55c89dafea46e007562c1879924
Tim Kim <timk@adobe.com>
2024-10-31 12:54:23 -0700

    Update onthispage.js

--- hlx_statics/blocks/onthispage/onthispage.js
+++ hlx_statics/blocks/onthispage/onthispage.js
@@ -13,7 +13,7 @@ export default async function decorate(block) {
     block.append(aside);
 
     const mainContainer = document.querySelector('main');
-    const headings = mainContainer.querySelectorAll('h2:not(.sid-nav h2):not(footer h2), h3:not(.sid-nav h3):not(footer h3)');
+    const headings = mainContainer.querySelectorAll('h2:not(.side-nav h2):not(footer h2), h3:not(.side-nav h3):not(footer h3)');
 
     Object.assign(aside.style, {
         display: 'flex',
