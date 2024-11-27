ca7ddba7187ffaee67ff52887a9f3e84e932767f
BaskarMitrah <mit44186@adobe.com>
2024-11-27 09:25:00 +0530

    Rmoved the unwanted code

--- hlx_statics/blocks/code/code.js
+++ hlx_statics/blocks/code/code.js
@@ -1,14 +1,6 @@
 import decoratePreformattedCode from '../../components/code.js';
 
 export default function decorate(block) {
-  // const codeCssFilePath = "/hlx_statics/blocks/code/code.css"
-  // if (!document.querySelector(`link[href="${codeCssFilePath}"]`)) {
-  //   const link = document.createElement('link');
-  //   link.rel = 'stylesheet';
-  //   link.href = codeCssFilePath;
-  //   document.head.appendChild(link);
-  // }
-
   const pre = document.createElement('pre');
   const code = block.querySelector('code');
   code.parentElement.replaceChild(pre, code);
